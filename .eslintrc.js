eb380356fe009bc3f222b04f4eb1513d62c02108
hehuangz <hehuan@hzyushi.cn>
2020-03-10 11:43:41 +0800

    + init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,42 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        indent: ['error', 4], // 4个空格缩进
+        'space-infix-ops': 'error', // 要求操作符周围有空格
+        'comma-spacing': 'error', // 逗号后面加空格
+        'arrow-spacing': 'error', // 使用 => 前后空格
+        'spaced-comment': 'warn', // 注释风格，前后留一个空格
+        'no-irregular-whitespace': 'error', // 不能有不规则的空格
+        'no-mixed-spaces-and-tabs': 'error', // 不能混用空格和tab
+        'no-multiple-empty-lines': 'error', // 不能有多余的空行
+        'no-var': 'error', // 不能使用var
+        'no-new': 'off',
+        'new-cap': 'off', // 不要求构造函数首字母大写
+        eqeqeq: 'error', // 使用全等
+        quotes: [1, 'single'], // 引号类型，使用单引号
+        camelcase: 'off'
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
 }
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
+<div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-  </div>
+</div>
 </template>
 
 <style lang="less">

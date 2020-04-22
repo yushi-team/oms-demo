4eadd36497e7a01646c818f8ede1e96f7a658fa5
hehuangz <1025005090@qq.com>
2020-04-22 10:02:21 +0800

    conf: 修改网络配置

--- src/api/config.js
+++ src/api/config.js
@@ -1,24 +1,24 @@
 // 不同环境的请求配置
 const prodConfigs = {
-    'app.wogame-dev': {
-        apiHost: 'http://appservice.wogame-dev.com',
-        domain: 'http://app.wogame-dev.com',
-        staticHost: '',
-        vconsole: true
+    'app.wogame-dev.com': {
+        apiHost: 'http://appservice.wogame-dev.com'
     },
-    'app.wogame-test': {
-        apiHost: 'http://appservice.wogame-test.com',
-        domain: 'http://app.wogame-test.com',
-        staticHost: '',
-        vconsole: false
+    'app.wogame-test.com': {
+        apiHost: 'http://appservice.wogame-test.com'
     }
 }
-// 本地默认
-const devConfig = {
-    apiHost: 'http://yapi.youximao.cn/mock/613',
-    domain: 'http://app.wogame-dev.com',
-    staticHost: '',
-    vconsole: true
+
+// 线上环境默认的配置
+const defaultConfig = {
+    apiHost: 'http://appservice.wogame-test.com'
 }
-const hostConfig = prodConfigs[location.hostname] || devConfig
+
+// 本地默认
+const localConfig = {
+    // apiHost: 'http://appservice.wogame-dev.com',
+    apiHost: 'http://yapi.youximao.cn/mock/613'
+}
+let hostConfig = prodConfigs[location.hostname] || defaultConfig
+if (process.env.NODE_ENV === 'development') hostConfig = localConfig
+
 export default hostConfig
